4189d94f4b1f77a0b3052eb1e035bea386261166
Frede94 <32297979+Frede94@users.noreply.github.com>
2022-09-14 10:06:16 +0200

    Update Program.cs

--- HotelBooking.WebApi/Program.cs
+++ HotelBooking.WebApi/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddScoped<IBookingManager, BookingManager>();
 builder.Services.AddTransient<IDbInitializer, DbInitializer>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about  configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
